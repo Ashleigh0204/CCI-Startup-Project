130380f51903dd2c96cbda48a4fc5b93f140aa96
ToyaOkey <cooltoya12@gmail.com>
2025-10-17 21:21:16 -0400

    feat: added mongodb

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
     const express = require('express');
     const index = require('./endpoints/index');
+    const dotenv = require('dotenv');
+    dotenv.config();
+    const mongoose = require('mongoose');
 
     //create app
     const app = express();
+
+
+    const mongoUri = process.env.MONGO_URI;
+    console.log(mongoUri);
+    mongoose.connect(mongoUri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log('Connected to MongoDB successfully');
+    })
+    .catch(err => {
+        console.error('MongoDB connection error:', err.message);
+        console.error('Full error:', err);
+    });
+
 
     //set up host and port
     const hostname = '127.0.0.1';
