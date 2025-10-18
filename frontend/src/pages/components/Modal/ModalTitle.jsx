e9227f521cb3d5c6b10b3dfe9f2b76d6507ff4b5
Ashleigh Sico <asico@uncc.edu>
2025-10-18 16:13:52 -0400

    added budget modals

---
+++ frontend/src/pages/components/Modal/ModalTitle.jsx
@@ -0,0 +1,7 @@
+export default function ModalTitle({children}) {
+    return (
+         <div className="text-lg text-green-800 font-bold">
+            {children}
+        </div>
+    )
+}
--- frontend/src/pages/index/Budget.jsx
+++ frontend/src/pages/index/Budget.jsx
@@ -1,16 +1,27 @@
 import Button from "../components/Button"
+import { useState } from "react"
+import ViewTransactionModal from "./ViewTransactionModal";
+import AdjustBudgetModal from "./AdjustBudgetModal";
+import AddTransactionModal from "./AddTransactionModal";
+
 export default function Budget({spent, budget, timeUnit}) {
+    const [viewTransactionsModalOpen, setViewTransactionModalOpen] = useState(false);
+    const [adjustBudgetModalOpen, setAdjustBudgetModalOpen] = useState(false);
+    const [addTransactionModalOpen, setAddTransactionModalOpen] = useState(false);
     return (
         <div>
             <p className="text-lg">You have spent ${spent} this {timeUnit}.</p>
             <p>You have ${budget-spent} left in your budget.</p>
             <div className="flex justify-center">
-                <Button className="w-full m-5">Add Transaction</Button>
+                <Button className="w-full m-5" onClick={() => setAddTransactionModalOpen(true)}>Add Transaction</Button>
             </div>
             <div className="flex justify-between mt-3">
-                <Button>View Transactions</Button>
-                <Button>Adjust Budget</Button>
+                <Button onClick={() => setViewTransactionModalOpen(true)}>View Transactions</Button>
+                <Button onClick={() => setAdjustBudgetModalOpen(true)}>Adjust Budget</Button>
             </div>
+            {viewTransactionsModalOpen && <ViewTransactionModal onRequestClose={()=> setViewTransactionModalOpen(false)} onSubmit={() => setViewTransactionModalOpen(false)} cancel={false}/>}
+            {adjustBudgetModalOpen && <AdjustBudgetModal onRequestClose={() => setAdjustBudgetModalOpen(false)} onSubmit={() => setAdjustBudgetModalOpen(false)} cancel={true} />}
+            {addTransactionModalOpen && <AddTransactionModal onRequestClose={() => setAddTransactionModalOpen(false)} onSubmit={() => setAddTransactionModalOpen(false)} cancel={true} locations={["Wendy's", "Chick-fil-A", "Crown Restaurant"]} />}
         </div>
     )
 }
