cc23a9a74b8b773623b4cc1b3b0fa4f558e533d7
Ashleigh Sico <asico@uncc.edu>
2025-10-18 20:53:29 -0400

    fix

--- frontend/src/pages/index/Recipe/Recipe.jsx
+++ frontend/src/pages/index/Recipe/Recipe.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Button from "../../../components/Button.jsx";
 import ViewRecipeModal from "./ViewRecipeModal";
 import BuyIngredientsModal from "./BuyIngredientsModal";
