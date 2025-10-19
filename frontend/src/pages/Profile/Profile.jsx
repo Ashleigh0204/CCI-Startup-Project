743400391a9cb21c8aeb6e36a1e5fe9f6aced3c9
Ashleigh Sico <asico@uncc.edu>
2025-10-18 21:06:07 -0400

    hot fix 3

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -252,8 +252,7 @@ export default function Profile() {
                                         <span className="text-3xl font-bold text-green-800">
                                             ${userData.budgetAmount?.toFixed(2)}
                                         </span>
-                                        <span className="text-lg text-gray-600 capitalize"> Per {" "}
-                                            {userData.timeUnit === 'Daily' ? 'day' : userData.timeUnit === 'weekly' ? 'Week' : 'Month'}
+                                        <span className="text-lg text-gray-600 capitalize"> 
                                         </span>
                                     </div>
                                 </div>
